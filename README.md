4dfb830327c43a829882fd6b8dadefef49776609
torvald2 <valis.source@gmail.com>
2023-06-30 10:24:36 +0300

    fix

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ The architecture of the application is depicted in the image below:
 
 Where:
 
-- Client is vue UI, whitch uses for upload photo, iterates with recognizer backend for get faces vectors. This vectors will stored in KWil decentralised database using Kwill JS SDK 
-- Metamask uses for authorisation and sign KWill transactions 
+- Client is vue UI, whitch uses for upload photo, iterates with recognizer backend for get faces vectors. This vectors will stored in KWil decentralised database using Kwil JS SDK 
+- Metamask uses for authorisation and sign KWil transactions 
 - Recognizer backend - Golang application that uses dLib SDK for face recognition issues. There is main two methods - get face vectors from photos for create challenge and compare vectors in Euclidean space. 
 
 
---
+++ client/src/views/ConnectWallet.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+b-container
+ 
+  b-row
+    b-col.mt-2
+      b-img(:src="image" fluid )
+  b-row.mt-3
+    b-col
+      b-alert(show variant="success")
+        h1 READY TO CREATE YOUR OWN CHALLENGE? 
+  b-row.mt-3
+    b-col
+      b-button(variant="success" pill size="lg" @click="connect")
+        b-img(:src="metamask" rounded="circle" fluid height="60em" width="60em")
+        | CONNECT WALLET
+</template>
+
+<script>
+// @ is an alias to /src
+import image from "../assets/onboarding.png";
+import metamask from "../assets/metamask-icon.png";
+
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data(){
+    return {
+        image,
+        metamask,
+        account:""
+    }
+  },
+  methods:{
+    connect(){
+        console.log("here")
+        if (typeof window.ethereum !== "undefined") {
+        ethereum
+         .request({ method: "eth_requestAccounts" })
+         .then((accounts) => {
+           const account = accounts[0]
+           this.account = account
+
+        }).catch((error) => {
+           console.log(error, error.code);
+
+        
+       });
+   } else {
+       window.open("https://metamask.io/download/", "_blank");
+   }
+    }
+  }
+}
+</script>
